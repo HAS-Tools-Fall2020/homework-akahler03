abaa6b0620d3042a0c29aeaba238039082772b3b
Abigail <akahler@email.arizona.edu>
2020-10-05 11:36:04 -0700

    kahler_hw6 to submissions

--- Submissions/kahler_hw6.py
+++ Submissions/kahler_hw6.py
@@ -15,7 +15,7 @@ import datetime
 # ** MODIFY **
 # Set the file name and path to where you have stored the data
 filename = 'streamflow_week6.txt'
-filepath = os.path.join('data', filename)
+filepath = os.path.join('homework-akahler03\data', filename)
 print(os.getcwd())
 print(filepath)
 
